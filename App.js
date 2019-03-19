38b5c0022ed5f2119f2d8e0505de36c26ac6b70e
evgeniy <evgeniy.logvinov@cyoda.com>
2019-03-19 22:25:45 +0300

    init commit

---
+++ App.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import { Platform, StatusBar, StyleSheet, View, Button, Image } from 'react-native';
+import { AppLoading, Asset, Font, Icon } from 'expo';
+import AppNavigator from './navigation/AppNavigator';
+
+import { DrawerNavigator, createDrawerNavigator, createAppContainer } from 'react-navigation';
+
+class MyHomeScreen extends React.Component {
+  static navigationOptions = {
+    drawerLabel: 'Home',
+    drawerIcon: ({ tintColor }) => (
+      <Image
+        source={require('./assets/images/icon.png')}
+        // source={require('./assets/chats-icon.png')}
+        style={[styles.icon, {tintColor: tintColor}]}
+      />
+    ),
+  };
+
+  render() {
+    return (
+      <Button
+        onPress={() => this.props.navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    );
+  }
+}
+
+class MyNotificationsScreen extends React.Component {
+  static navigationOptions = {
+    drawerLabel: 'Notifications',
+    drawerIcon: ({ tintColor }) => (
+      <Image
+        source={require('./assets/images/icon.png')}
+        style={[styles.icon, {tintColor: tintColor}]}
+      />
+    ),
+  };
+
+  render() {
+    return (
+      <Button
+        onPress={() => this.props.navigation.goBack()}
+        title="Go back home"
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  icon: {
+    width: 24,
+    height: 24,
+  },
+});
+
+const MyDrawerNavigator = createDrawerNavigator({
+  Home: {
+    screen: MyHomeScreen,
+  },
+  Notifications: {
+    screen: MyNotificationsScreen,
+  },
+});
+
+const MyApp = createAppContainer(MyDrawerNavigator);
+
+// const App = DrawerNavigator({
+//   Home: {
+//     screen: MyHomeScreen,
+//   },
+//   Notifications: {
+//     screen: MyNotificationsScreen,
+//   },
+// });
+
+export default MyApp;
+// export default class App extends React.Component {
+//   state = {
+//     isLoadingComplete: false,
+//   };
+
+//   render() {
+//     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+//       return (
+//         <AppLoading
+//           startAsync={this._loadResourcesAsync}
+//           onError={this._handleLoadingError}
+//           onFinish={this._handleFinishLoading}
+//         />
+//       );
+//     } else {
+//       return (
+//         <View style={styles.container}>
+//           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+//           <AppNavigator />
+//         </View>
+//       );
+//     }
+//   }
+
+//   _loadResourcesAsync = async () => {
+//     return Promise.all([
+//       Asset.loadAsync([
+//         require('./assets/images/robot-dev.png'),
+//         require('./assets/images/robot-prod.png'),
+//       ]),
+//       Font.loadAsync({
+//         // This is the font that we are using for our tab bar
+//         ...Icon.Ionicons.font,
+//         // We include SpaceMono because we use it in HomeScreen.js. Feel free
+//         // to remove this if you are not using it in your app
+//         'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+//       }),
+//     ]);
+//   };
+
+//   _handleLoadingError = error => {
+//     // In this case, you might want to report the error to your error
+//     // reporting service, for example Sentry
+//     console.warn(error);
+//   };
+
+//   _handleFinishLoading = () => {
+//     this.setState({ isLoadingComplete: true });
+//   };
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//   },
+// });
